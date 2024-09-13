08f64f3a6105d8c4888e2efc3e1adf019b3d31f4
Kampagrinder <kampagrinder@gmail.com>
2024-09-13 09:43:12 +0300

    now we draw player!

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from constants import *
+from player import Player
 import pygame
-fps = pygame.time.Clock()
-dt = 0 
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    fps = pygame.time.Clock()
+    dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return    
         screen.fill(pygame.Color("black"))
+        player.draw(screen)   
         pygame.display.flip()
         dt = fps.tick(60) / 1000
-
+        
 
 if __name__ == "__main__":
     main()
 
 
-#  git push origin main -git hub commit
+#  git push origin main - git hub commit
 #  source venv/bin/activate - virtual env
