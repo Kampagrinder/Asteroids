002da3c2713d44463db7ed2f1a02587d8a524825
Kampagrinder <kampagrinder@gmail.com>
2024-09-16 14:56:34 +0300

    asteroid spawn

---
+++ asteroid.py
@@ -0,0 +1,17 @@
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.velocity = pygame.Vector2(0, 0)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen,"white" ,(self.x, self.y), self.radius, width=2)
+     
+    def update(self, dt):
+        self.x += self.velocity.x * dt
+        self.y += self.velocity.y * dt
+
